a90715f602c137aa845bf6624f8573f6011edb37
Lana Nguyen <lana.nguyen.t@gmail.com>
2021-04-09 23:26:37 -0500

    Login and register with object dto added

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -31,6 +31,17 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
             return user;
         }
+        [HttpPost("Login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+            var user = await _context.Users.SingleOrDefaultAsync( y => y.UserName == loginDto.Username.ToLower());
+            if (user == null) return Unauthorized("invalid user");
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash  = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            for (int i=0; i < computedHash.Length; i++){
+                if(computedHash[i] != user.PasswordHash[i])return Unauthorized("wrong password");
+            }
+            return user;
+        }
         private async Task<bool> UserExists(string userName){
             return await _context.Users.AnyAsync(x =>x.UserName == userName.ToLower());
         }
